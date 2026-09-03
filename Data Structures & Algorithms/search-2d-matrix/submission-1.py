9e7ffdc6f010beaacf06d2878f161aefc5bf665a
yashj846 <44563303+yashj846@users.noreply.github.com>
2026-09-02 19:19:15 -0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,56 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        for m in matrix:
+            if m[0] <= target <= m[len(m)-1]:
+                start, end = 0, len(m) -1
+                while start  <= end:
+                    mid = (start + end) // 2
+                    if target < m[mid]:
+                        end = mid - 1
+                    elif target > m[mid]:
+                        start = mid + 1
+                    else:
+                        return True
+        return False
+
+
+
+        # start, end = 0, len(matrix[0]) - 1
+        # while start <= end:
+        #     mid = (start + end) // 2
+        #     if target < matrix[mid]:
+                
+
+        #     elif target > m[mid]:
+
+        #     else:
+
+
+
+        # row_start, row_end = 0, len(matrix)-1
+        # col_start,col_end = 0, len(matrix[0]) - 1
+
+        # while row_start <= row_end:
+        #     row_mid = (row_start + row_end) // 2
+        #     while col_start <= col_end:
+        #         col_mid = (col_end + col_start) // 2
+        #         if target < matrix[row_mid][col_mid]:
+        #             col_end = col_mid - 1
+        #         elif target > matrix[row_mid][col_mid]:
+        #             col_start = col_mid + 1
+        #         else:
+        #             return True
+        #     if target < matrix[row_mid][col_mid]:
+        #         row_end = row_mid - 1
+        #     elif target > matrix[row_mid][col_mid]:
+        #         row_start = row_mid + 1
+        #     else:
+        #         return True 
+        # return False
+            
+    
+
+
+        
+        
