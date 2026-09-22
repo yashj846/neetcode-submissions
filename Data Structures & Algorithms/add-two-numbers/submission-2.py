0d110419ce9e0a847d38d17b37ec2f06fd781168
yashj846 <44563303+yashj846@users.noreply.github.com>
2026-09-21 20:16:42 -0700

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        array_n = []
+        temp = ListNode(0)
+        ans = temp 
+        while l1 and l2:
+            add = (l1.val + l2.val + carry) % 10
+            carry =  (l1.val + l2.val + carry) // 10
+            temp.next = ListNode(add)
+            temp = temp.next
+            # add_n = ListNode(add)
+            # array_n.append(add_n)
+            l1 = l1.next
+            l2 = l2.next
+
+        while l1:
+            add = (l1.val + carry) % 10
+            carry =  (l1.val + carry) // 10
+            temp.next = ListNode(add)
+            temp = temp.next
+            l1 = l1.next
+
+        while l2:
+            add = (l2.val + carry) % 10
+            carry =  (l2.val + carry) // 10
+            temp.next = ListNode(add)
+            temp = temp.next
+            l2 = l2.next
+
+        # for i in range(1, len(array_n), 1):
+        #     array_n[i-1].next = array_n[i]
+        
+        if carry != 0:
+            temp.next = ListNode(carry, None)
+        
+        return ans.next
+            
+        
+
